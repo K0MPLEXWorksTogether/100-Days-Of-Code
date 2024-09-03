7f5cc88c8e515f327543e7fdc2c58d50e48dd556
K0MPLEXOnCodeberg <k0mplexontuta@tutamail.com>
2024-09-03 21:15:59 +0530

    Created a quiz game using turtle and pandas that allows you to guess all the states of the US.

---
+++ USStatesGame/main.py
@@ -0,0 +1,38 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("US States Game")
+screen.setup(height=600, width=800)
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+us_states_data = pandas.read_csv("50_states.csv")
+us_states = us_states_data.state.to_list()
+
+writer = turtle.Turtle()
+writer.hideturtle()
+writer.penup()
+
+
+guessed_states = []
+while len(guessed_states) < 50:
+	guess = turtle.textinput(title=f"[{len(guessed_states)}/50] States Correct", prompt="Guess a state that belongs to the US.").title()
+	if guess == "Exit":
+		break
+	if guess in us_states:
+		guessed_states.append(guess)
+		state_row = us_states_data[us_states_data.state == guess]
+		writer.goto(state_row.x.item(), state_row.y.item())
+		writer.write(guess)
+
+missed_states = []
+for state in us_states:
+	if state not in guessed_states:
+		missed_states.append(state)
+
+missed_data = pandas.Series(missed_states)
+missed_data.to_csv("missed.csv")
+
+screen.exitonclick()
