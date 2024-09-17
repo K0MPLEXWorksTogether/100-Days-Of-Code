883810276eb9b73f3f879211b3aee022a98c0115
K0MPLEXWorksTogether <abhirammangipudi03@gmail.com>
2024-09-17 23:09:52 +0530

    Implemented a safe exception handling mechanism for user input.

--- NATOAlphabet/main.py
+++ NATOAlphabet/main.py
@@ -2,6 +2,14 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 code_dict = {row.letter:row.code for (index, row) in data.iterrows()} 
 
-user_name = input("Enter Your Text: ").upper()
-code_user = [code_dict[char] for char in user_name]
-print(code_user)
+def get_input():
+    try:
+        user_name = input("Enter Your Text: ").upper()
+        code_user = [code_dict[char] for char in user_name]
+        print(code_user)
+    except KeyError:
+        print("Sorry, Only letters in the alphabet please.")
+        get_input()
+
+get_input()
+
