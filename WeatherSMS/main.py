e11b0d452f7ea414f4f01f6d6a0427147723e4ab
K0MPLEXWorksTogether <abhirammangipudi03@gmail.com>
2024-09-26 10:18:07 +0530

    Made some small changes to the WeatherAPI JSON Extraction.

--- WeatherSMS/main.py
+++ WeatherSMS/main.py
@@ -1,12 +1,12 @@
 import requests
 from twilio.rest import Client
 
-LATITUDE = input("Enter Your Latitude: ") + ","
-LONGITUDE = input("Enter Your Longitude: ")
+LATITUDE = input("Enter Your Latitude")
+LONGITUDE = input("Enter Your Longitude")
 API_KEY = input("Enter Your WeatherAPI Key: ")
 ENDPOINT = input("Enter Your WeatherAPI Endpoint: ")
-ACCOUNT_SID = input("Enter Your Twilio Account SID: ")
-AUTH_TOKEN = input("Enter Your Twilio Auth Token: ")
+ACCOUNT_SID = input("Enter Your Twillio Account SID: ")
+AUTH_TOKEN = input("Enter Your Twilio Auth Token")
 
 parameters = {
     "key": API_KEY,
@@ -18,25 +18,12 @@ response = requests.get(ENDPOINT, params=parameters)
 response.raise_for_status()
 
 weather_data = response.json()
-rain_chance = weather_data["forecast"]["forecastday"][0]["hour"][16]["will_it_rain"]
+weather_condition = weather_data["forecast"]["forecastday"][0]["hour"][16]["condition"]["text"]
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
-
-count = 0
-while count <= 3:
-    if int(rain_chance):
-        message = client.messages.create(
-        body="Be Prepared. It might rain today at 4pm.",
-        from_="+12672143030",
-        to="+919701279950",
-        )
-        print("Sent Positive Rain Message")
-        count += 1
-    else:
-        message = client.messages.create(
-        body="It will not rain today at 4pm. You can chill :)",
-        from_="+12672143030",
-        to="+919701279950",
-        )
-        print("Sent Negative Rain Message")
-        count += 1
+message = client.messages.create(
+    body=f"Weather Conditions At Dundigal At 4PM: {weather_condition}",
+    from_="+12672143030",
+    to="+919701279950",
+)
+print("Sent Message!")
